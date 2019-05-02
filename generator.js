3411b5499a16b98c3257f0f478b645e5504dc51f
Alonso Júnior <alonsorsjunior@gmail.com>
2019-05-02 10:15:58 -0300

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,65 @@
+// ALONSO JR. 
+// https://github.com/alonsojr1980
+
+// MONGODB SCHEMA FOR EASY SEQUENCIAL ID GENERATION
+
+// USAGE EXAMPLE:
+
+/*
+var Generator = require('../models/generator.js');
+
+Generator.gen_id("quotation_" + quotation.sp_id, 1, function (err3, seq) {
+    if (!err3) {
+        quote.sp_sequence = seq;                  
+        quote.save(function(error){
+            if (!error) {
+                callback(null, quote);
+            } else {
+                callback(error, null);
+            }
+        });
+    }
+});
+
+*/
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GeneratorSchema = new Schema({
+    name: String,
+    sequence: Number
+}, { collection: 'generators' });
+
+GeneratorSchema.methods = {
+    incrementAndSave: function (increment, callback) {
+        var that = this;
+        that.sequence += increment;
+        that.save(function(err) {
+            if (!err) {
+                callback(null, that.sequence);
+            } else {
+                callback(err, null);
+            }
+        });
+    }
+};
+
+GeneratorSchema.statics = {
+
+    gen_id: function (name, increment, callback) {
+        var gen = this.findOne({name: name}, function(err, generator){
+            if (!err) {
+                if (!generator) {
+                    var model = mongoose.model('Generator', GeneratorSchema);
+                    generator = model({name: name, sequence: 0});
+                }
+
+                generator.incrementAndSave(increment, callback);
+            }
+        });
+    }
+
+}
+
+module.exports = mongoose.model('Generator', GeneratorSchema);;
